8b78cbef0de2e697bf6b8b02aea7e1d27523260a
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-08-26 15:11:54 -0700

    fixed calculation type error in Zeo++ submision script.

--- descriptors/submit_zeo_calculations.sh
+++ descriptors/submit_zeo_calculations.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 echo "#### START #### " `date`
+
 probe_radius=0.31 # atomic radius of He probe in Angstroms
 num_samples=5000  # number of Monte Carlo integration samples
 crystals_loc=../data/crystals
-output_loc=./zeo_outputs
 
+output_loc=./zeo_outputs
+mkdir -p $output_loc
 
 for xtal in $(cat ./tmp_cof_names.txt)
 do
     ~/zeo++-0.3/network -stripatomnames -ha -res $output_loc/$xtal.res \
-                        -chan $probe_radius $output_loc/$xtal.chan \
+                        -vol $probe_radius $probe_radius 10*$num_samples $output_loc/$xtal.vol \
                         -sa $probe_radius $probe_radius $num_samples $output_loc/$xtal.sa \
                         $crystals_loc/$xtal
 done
 
 # compile results into summary files
-cat *.res > summary_pore_diameters.txt
-cat *.chan > summary_void_fractions.txt
-grep "@" *.sa > summary_surface_areas.txt
+cat ./$output_loc/*.res > ./summary_pore_diameters.txt
+grep "@" ./$output_loc/*.vol > ./summary_void_fractions.txt
+grep "@" ./$output_loc/*.sa > ./summary_surface_areas.txt
 
 echo "#### END ####" `date`
 
