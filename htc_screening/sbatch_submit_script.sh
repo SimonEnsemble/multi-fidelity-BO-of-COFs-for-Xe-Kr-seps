682554e2da349e71438c8140442bfdeedeb89750
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-09-23 21:58:32 -0700

    found bug in analysis notebook when constructing dataframes causing GP to fail. plotted relative error in benchmark and determined to use 500 insertions/cycles per volume to get less than 0.05 relative error in materials.

--- htc_screening/sbatch_submit_script.sh
+++ htc_screening/sbatch_submit_script.sh
@@ -13,16 +13,10 @@ do
     esac
 done
 
-if $is_henry
-then
-    ins_per_vol=500 # insertions per volume
-else
-    ins_per_vol=150 # number of MC cycles per volume
-fi
-
+ins_per_vol=500 # insertions (cycles) per volume
 
 # loop over the xtal names in AA_mofs_to_sim.txt
-for xtal in 07012N3_ddec.cif #$(cat ./AA_cofs_to_sim.txt)
+for xtal in $(cat ./AA_cofs_to_sim.txt)
 do
     sleep 0.25 # don't overwhelm cluster
 
@@ -39,7 +33,7 @@ do
 
     echo "submitting job for $xtal with $ins_per_vol cycles/insertions per volume"
     sbatch -J "$xtal-$ins_per_vol-$ljff" -A simon-grp -p mime5 -n 1 \
-        --mail-type=END,FAIL --mail-user=gantzlen \
+        --time=2-00:00:00 --mail-type=END,FAIL --mail-user=gantzlen \
         -o $sim_log_loc/"$xtal-$ins_per_vol-$ljff.o" \
         -e $sim_log_loc/"$xtal-$ins_per_vol-$ljff.e" \
         --export=xtal="$xtal",ins_per_vol="$ins_per_vol" simulation_submit.sh $is_henry
