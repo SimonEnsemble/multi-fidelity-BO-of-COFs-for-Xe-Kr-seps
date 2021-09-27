beb929124475f6d4a62aade6144ef342304f560d
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-09-27 08:25:21 -0700

    reran some Henry sims that failed before.

--- htc_screening/sbatch_submit_script.sh
+++ htc_screening/sbatch_submit_script.sh
@@ -16,7 +16,7 @@ done
 ins_per_vol=500 # insertions (cycles) per volume
 
 # loop over the xtal names in AA_mofs_to_sim.txt
-for xtal in $(cat ./AA_cofs_to_sim.txt)
+for xtal in 07012N3_ddec.cif 14040N2_ddec.cif 16290N3_ddec.cif 21111N3_ddec.cif  #$(cat ./AA_cofs_to_sim.txt)
 do
     sleep 0.25 # don't overwhelm cluster
 
@@ -33,7 +33,7 @@ do
 
     echo "submitting job for $xtal with $ins_per_vol cycles/insertions per volume"
     sbatch -J "$xtal-$ins_per_vol-$ljff" -A simon-grp -p mime5 -n 1 \
-        --time=2-00:00:00 --mail-type=END,FAIL --mail-user=gantzlen \
+        --time=7-00:00:00 --mail-type=END,FAIL --mail-user=gantzlen \
         -o $sim_log_loc/"$xtal-$ins_per_vol-$ljff.o" \
         -e $sim_log_loc/"$xtal-$ins_per_vol-$ljff.e" \
         --export=xtal="$xtal",ins_per_vol="$ins_per_vol" simulation_submit.sh $is_henry
